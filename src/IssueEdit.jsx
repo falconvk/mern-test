736cadde3ad5446d3cc9d96c620d155db2c98723
Nikola Rajkovic <falconvk@gmail.com>
2017-07-04 04:10:02 +0200

    Implement Issue update FE functionality

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -16,12 +16,13 @@ export default class IssueEdit extends React.Component {
         owner: '',
         effort: null,
         completionDate: null,
-        created: '',
+        created: null,
       },
       invalidFields: {},
     };
     this.onChange = this.onChange.bind(this);
     this.onValidityChange = this.onValidityChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -51,13 +52,43 @@ export default class IssueEdit extends React.Component {
     this.setState({ invalidFields });
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+    if (Object.keys(this.state.invalidFields).length) {
+      return;
+    }
+
+    fetch(`/api/issues/${this.props.match.params.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state.issue),
+    })
+      .then((response) => {
+        if (response.ok) {
+          response.json()
+            .then((updatedIssue) => {
+              updatedIssue.created = new Date(updatedIssue.created);
+              if (updatedIssue.completionDate) {
+                updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+              }
+              this.setState({ issue: updatedIssue });
+              alert('Updated issue successfully.');
+            });
+        } else {
+          response.json()
+            .then(error => alert(`Failed to update issue: ${error.message}`));
+        }
+      })
+      .catch(err => alert(`Error in sending data to server: ${err.message}`));
+  }
+
   loadData() {
     fetch(`/api/issues/${this.props.match.params.id}`)
       .then((response) => {
         if (response.ok) {
           response.json()
             .then((issue) => {
-              issue.created = new Date(issue.created).toDateString();
+              issue.created = new Date(issue.created);
               issue.completionDate = issue.completionDate
                 ? new Date(issue.completionDate)
                 : null;
@@ -78,10 +109,10 @@ export default class IssueEdit extends React.Component {
       : (<div className="error">Please correct invalid fields before submitting.</div>);
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           ID: {issue._id}
           <br />
-          Created: {issue.created}
+          Created: {issue.created ? issue.created.toDateString() : ''}
           <br />
           Status:
           <select
